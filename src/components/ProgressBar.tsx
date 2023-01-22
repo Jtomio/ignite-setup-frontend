8aab13a14f54c5619210582efa3b5928e888b5d9
Jeison Tomio <medriase@gmail.com>
2023-01-22 02:27:36 -0300

    fixed progressBar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -7,7 +7,7 @@ interface ProgressBarProps {
 export function ProgressBar(props: ProgressBarProps) {
 
   return (
-    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+    <div className="h-3 max-w-full rounded-xl bg-zinc-700 w-full mt-4">
       <div
         role="progressbar"
         aria-label="Progresso de hábitos completados nesse dia."
